2e5420521d636b77d227e1453dd7afef023b4d6f
Joe Simoes <joe@generalassemb.ly>
2014-02-16 18:16:15 -0500

    Test celcius to fahrenheit

---
+++ js/app/utils.js
@@ -0,0 +1,23 @@
+define([
+	'app/utils'
+], function(Utils) {
+	'use strict';
+
+	var isNumber = function(n) {
+		return !isNaN(parseFloat(n)) && isFinite(n);
+	}
+
+	var celsiusToFahrenheit = function(cel) {
+		if (!isNumber(cel)) {
+			return null;
+		}
+
+		var fahr = (cel * 9 / 5) + 32;
+		return fahr;
+	};
+
+	return {
+		celsiusToFahrenheit: celsiusToFahrenheit
+	};
+
+});
