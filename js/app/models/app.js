1f2af053aa4e36bcbcfcc39eb1b36aa0127274b8
Joe Simoes <joe@generalassemb.ly>
2014-02-17 20:36:46 -0500

    Add LocalStorage, place model and places collection

--- js/app/models/app.js
+++ js/app/models/app.js
@@ -1,9 +1,11 @@
 define([
 	'backbone',
+	'backbone.localStorage'
 ], function (Backbone) {
 	'use strict';
 
 	var AppModel = Backbone.Model.extend({
+		localStorage: new Backbone.LocalStorage("AppSettings"),
 		defaults: {
 			'backgroundColor': '#999999',
 			'celcius': true,
